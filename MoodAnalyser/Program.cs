2c9233727e95cc95828276fcc543036b07958818
Mohanee <mohanee99@gmail.com>
2020-10-13 21:14:03 +0530

    [mohanee] add code for mood analyser

--- MoodAnalyser/Program.cs
+++ MoodAnalyser/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace MoodAnalyser
 {
@@ -7,6 +8,33 @@ namespace MoodAnalyser
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Welcome to Mood Analyser Problem");
+            Console.WriteLine("What is your mood??");
+            string message = Console.ReadLine();
+            MoodAnalyser ma = new MoodAnalyser();
+            ma.CheckMood(message);
+             
+        }
+    }
+
+    class MoodAnalyser
+    {
+        public void CheckMood(string message)
+        {
+            string msg = message.ToLower();
+
+            if(msg == "sad")
+            {
+                Console.WriteLine("Ohh, you're in a sad mood");
+            }
+            else if(msg == "happy")
+            {
+                Console.WriteLine("Wow, you're in a happy mood");
+            }
+
+            else
+            {
+                Console.WriteLine("Sorry, unable to identify your mood");
+            }
         }
     }
 }
