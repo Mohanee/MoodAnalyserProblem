f186d76f14bfd0eea73f69396347f5f0a2aeecc0
Mohanee <mohanee99@gmail.com>
2020-10-14 12:13:26 +0530

    [mohanee] add test method for Mood Analyser using Constructor

--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -10,12 +10,30 @@ namespace MoodAnalyserTest
         [TestMethod]
         public void IAmSadMood_Returns_SAD_TC1()
         {
-            MoodAnalyserClass ma = new MoodAnalyser.MoodAnalyserClass();
-            string message = "I am in sad mood";
 
-            string result= ma.CheckMood(message);
+            string message = "I am in sad mood";
+            MoodAnalyserClass ma = new MoodAnalyser.MoodAnalyserClass(message);
+            string result= ma.CheckMood();
 
             Assert.AreEqual(result,"SAD");
+        }
+
+        [TestMethod]
+        public void IAminAnyMood_Returns_HAPPY_TC2()
+        {
+            string message = "I am in any mood";
+            MoodAnalyserClass ma = new MoodAnalyser.MoodAnalyserClass(message);
+            string result = ma.CheckMood();
+            Assert.AreEqual(result, "HAPPY");
+        }
+
+        [TestMethod]
+        public void IamSadMood_Returns_SAD_usingCtor()
+        {
+            MoodAnalyserClass ma = new MoodAnalyser.MoodAnalyserClass("I am in sad mood");
+            string result = ma.CheckMood();
+
+            Assert.AreEqual(result, "SAD");
         }
     }
 }
