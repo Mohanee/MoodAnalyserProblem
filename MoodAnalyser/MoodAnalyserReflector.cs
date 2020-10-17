a5f8e6a4af56a6d2c00871423220b6efb93e0ace
Mohanee <mohanee99@gmail.com>
2020-10-17 19:32:12 +0530

    [mohanee] add new mood analyser reflector file to invoke check mood method

---
+++ MoodAnalyser/MoodAnalyserReflector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace MoodAnalyser
+{
+    class MoodAnalyserReflector
+    {
+        public static string InvokeAnalyseMood(string message, string methodName)
+        {
+            try
+            {
+                Type type = Type.GetType("MoodAnalyser.MoodAnalyserClass");
+                object moodAnalyserObject = MoodAnalyserFactory.CreateMoodAnalyserUsingParameterisedConstructor("MoodAnalyser.MoodAnalyserClass", "MoodAnalyserClass", message);
+                MethodInfo method = type.GetMethod(methodName);
+                object mood = method.Invoke(moodAnalyserObject, null);
+                return mood.ToString();
+            }
+            catch (NullReferenceException e)
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.METHOD_NOT_FOUND, "No such method found");
+            }
+        }
+    }
+}
